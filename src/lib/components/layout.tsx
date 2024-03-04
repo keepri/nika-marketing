cd39fbbf4d49e5321d2fe376e53fe1808f0dca6b
KIPRI <hi@kipri.com>
2024-03-04 16:56:07 +0200

    style: layout component fine tunning

--- src/lib/components/layout.tsx
+++ src/lib/components/layout.tsx
@@ -3,14 +3,19 @@ import { type ParentProps } from "solid-js";
 import { BaseHtml } from "./base-html";
 import { Navbar } from "./navbar";
 
-type LayoutProps = ParentProps;
+type LayoutProps = ParentProps<{
+    class?: string;
+}>;
 
 export function Layout(props: LayoutProps) {
+    props.class ??= "";
+
     const safeChildren = props.children;
+    const className = props.class + " relative container px-4";
 
     return (
         <BaseHtml>
-            <body class="relative container px-4">
+            <body class={className}>
                 <Navbar />
 
                 {safeChildren}
--- src/lib/pages/home.tsx
+++ src/lib/pages/home.tsx
@@ -3,20 +3,18 @@ import { Layout } from "../components/layout";
 
 export function HomePage() {
     return (
-        <Layout>
-            <main class="grid items-center min-h-screen">
-                <div>
-                    <h1 class="max-w-[25ch] font-bold text-5xl">
-                        Probably the best digital decision you're about to make.
-                    </h1>
+        <Layout class="grid items-center min-h-screen">
+            <main>
+                <h1 class="max-w-[25ch] font-bold text-5xl">
+                    Probably the best digital decision you're about to make.
+                </h1>
 
-                    <p class="max-w-prose text-2xl my-8">
-                        We help small business owners establish a productive
-                        presence online and offline.
-                    </p>
+                <p class="max-w-prose text-2xl my-8">
+                    We help small business owners establish a productive
+                    presence online and offline.
+                </p>
 
-                    <Button>BOOK A DISCOVERY MEETING</Button>
-                </div>
+                <Button>BOOK A DISCOVERY MEETING</Button>
             </main>
         </Layout>
     );
